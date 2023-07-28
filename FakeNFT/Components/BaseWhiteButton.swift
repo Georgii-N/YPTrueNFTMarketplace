7cf06370ec7a9d3008faa5235c74a86bfde9c38d
Georgii <neguritsageorgiy@gmail.com>
2023-07-28 16:37:25 +0400

    поправил структуру папок, добавил пару базовых кнопок

---
+++ FakeNFT/Components/BaseWhiteButton.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+final class BaseWhiteButton: UIButton {
+    let labelText: String
+    
+    init(with title: String) {
+        self.labelText = title
+        super.init(frame: .zero)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        self.backgroundColor = Resources.Colors.whiteDay
+        
+        self.setTitle(labelText, for: .normal)
+        self.setTitleColor(Resources.Colors.blackDay, for: .normal)
+        
+        self.layer.cornerRadius = 16
+        self.layer.borderWidth = 1.0
+        self.layer.borderColor = Resources.Colors.blackDay.cgColor
+        
+        self.titleLabel?.textAlignment = .center
+        self.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        
+        NSLayoutConstraint.activate([
+            self.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+}
---
+++ FakeNFT/Components/BlackBaseButton.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+final class BaseBlackButton: UIButton {
+    let labelText: String
+    
+    init(with title: String) {
+        self.labelText = title
+        super.init(frame: .zero)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        self.backgroundColor = Resources.Colors.blackDay
+        self.setTitle(labelText, for: .normal)
+        self.setTitleColor(Resources.Colors.whiteDay, for: .normal)
+        self.layer.cornerRadius = 16
+        self.titleLabel?.textAlignment = .center
+        self.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        
+        NSLayoutConstraint.activate([
+            self.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+}
