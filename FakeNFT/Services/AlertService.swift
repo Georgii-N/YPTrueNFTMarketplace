082c3ced9ed7d3e0f15e1753910c1f6bcc269baa
esplusnin <esplusnin94@yandex.ru>
2023-07-28 10:45:51 +0400

    Добавил алерт сервис и цвета в ассеты

---
+++ FakeNFT/Services/AlertService.swift
@@ -0,0 +1,28 @@
+//
+//  AlertService.swift
+//  FakeNFT
+//
+//  Created by Евгений on 28.07.2023.
+//
+
+import UIKit
+
+final class AlertService {
+    
+        func showAlert(title: String?,
+                   message: String?,
+                   preferredStyle: UIAlertController.Style,
+                   controller: UIViewController,
+                   completion: @escaping () -> ()) {
+        let alert = UIAlertController(title: title ?? nil,
+                                      message: message ?? nil,
+                                      preferredStyle: preferredStyle)
+            let okAction = UIAlertAction(title: "OK", style: .default)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            alert.addAction(okAction)
+            alert.addAction(cancelAction)
+            
+            controller.present(alert, animated: true)
+    }
+}
