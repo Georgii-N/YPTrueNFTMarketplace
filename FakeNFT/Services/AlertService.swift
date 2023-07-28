dd81f2266d5094d308733eae0b2996b145e6d600
esplusnin <esplusnin94@yandex.ru>
2023-07-28 11:02:05 +0400

    Добавил ресурсы и цвета

--- FakeNFT/Services/AlertService.swift
+++ FakeNFT/Services/AlertService.swift
@@ -8,21 +8,21 @@
 import UIKit
 
 final class AlertService {
-    
+
         func showAlert(title: String?,
                    message: String?,
                    preferredStyle: UIAlertController.Style,
                    controller: UIViewController,
-                   completion: @escaping () -> ()) {
+                   completion: @escaping () -> Void) {
         let alert = UIAlertController(title: title ?? nil,
                                       message: message ?? nil,
                                       preferredStyle: preferredStyle)
             let okAction = UIAlertAction(title: "OK", style: .default)
             let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-            
+
             alert.addAction(okAction)
             alert.addAction(cancelAction)
-            
+
             controller.present(alert, animated: true)
     }
 }
---
+++ FakeNFT/Supportfiles/Resources /Resources .swift
@@ -0,0 +1,24 @@
+//
+//  Resources .swift
+//  FakeNFT
+//
+//  Created by Евгений on 28.07.2023.
+//
+
+import UIKit
+
+enum Resources {
+    enum Colors {
+        static let backgroundUniversal = UIColor(named: "Background Universal") ?? UIColor.black
+        static let blackDay = UIColor(named: "Black [day]") ?? UIColor.black
+        static let blackUniversal = UIColor(named: "Black Universal") ?? UIColor.black
+        static let blueUniversal = UIColor(named: "Blue Universal") ?? UIColor.blue
+        static let grayUniversal = UIColor(named: "Gray Universal") ?? UIColor.gray
+        static let greenUniversal = UIColor(named: "Green Universal") ?? UIColor.green
+        static let lightGrayDay = UIColor(named: "Light gray [day]") ?? UIColor.lightGray
+        static let redUniversal = UIColor(named: "Red Universal") ?? UIColor.red
+        static let whiteDay = UIColor(named: "White [day]") ?? UIColor.white
+        static let whiteUniversal = UIColor(named: "White Universal") ?? UIColor.white
+        static let yellowUniversal = UIColor(named: "Yellow Universal") ?? UIColor.yellow
+    }
+}
