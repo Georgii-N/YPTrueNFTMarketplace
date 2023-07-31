fce582cee221955df897a1a056ef7dea15d58a80
esplusnin <esplusnin94@yandex.ru>
2023-07-31 15:12:37 +0400

    Сверстал главный экран каталога. Таблица пока что на моковых данных

---
+++ FakeNFT/Scenes /Catalog/CatalogProtocols/CatalogViewModelProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  CatalogViewModelProtocol.swift
+//  FakeNFT
+//
+//  Created by Евгений on 31.07.2023.
+//
+
+import Foundation
---
+++ FakeNFT/Scenes /Catalog/CatalogTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  CatalogTableViewCell.swift
+//  FakeNFT
+//
+//  Created by Евгений on 31.07.2023.
+//
+
+import Foundation
