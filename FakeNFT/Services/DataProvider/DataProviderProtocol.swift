f0fae548a1e711977ef459a8e44d35aec49e6124
esplusnin <esplusnin94@yandex.ru>
2023-08-05 15:56:45 +0400

    Апдейт сервис, екстеншены

--- FakeNFT/Services/DataProvider/DataProviderProtocol.swift
+++ FakeNFT/Services/DataProvider/DataProviderProtocol.swift
@@ -2,4 +2,6 @@ import Foundation
 
 protocol DataProviderProtocol {
     func fetchUsersRating(completion: @escaping (Result<[User], Error>) -> Void)
+    func fetchProfileId(userId: String, completion: @escaping (Result<UserResponse, Error>) -> Void)
+    func fetchUsersNFT(userId: String, nftsId: [String]?, completion: @escaping (Result<NFTCards, Error>) -> Void)
 }
