46721bb94fcf98fb349f458d6c4cee9f99c2a2a3
Georgii <neguritsageorgiy@gmail.com>
2023-08-05 20:44:35 +0400

    обновил функцию для загрузки по 10 моделек юзеров для коллекции

--- FakeNFT/Services/DataProvider/DataProviderProtocol.swift
+++ FakeNFT/Services/DataProvider/DataProviderProtocol.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 protocol DataProviderProtocol {
-    func fetchUsersRating(completion: @escaping (Result<[User], Error>) -> Void)
+    func fetchUsersRating(page: Int, completion: @escaping (Result<[User], Error>) -> Void)
     func fetchProfileId(userId: String, completion: @escaping (Result<UserResponse, Error>) -> Void)
     func fetchUsersNFT(userId: String, nftsId: [String]?, completion: @escaping (Result<NFTCards, Error>) -> Void)
     func fetchCurrencies(completion: @escaping (Result<Currencies, Error>) -> Void)
