14d78991724a8d9c7433aa249e327bcf5e289e86
Georgii <neguritsageorgiy@gmail.com>
2023-08-11 19:22:42 +0400

    change button ui touches

--- FakeNFT/Components/Buttons/BaseWhiteButton.swift
+++ FakeNFT/Components/Buttons/BaseWhiteButton.swift
@@ -12,6 +12,21 @@ final class BaseWhiteButton: UIButton {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        transform = .identity
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        transform = .identity
+    }
 
     private func setupUI() {
         self.backgroundColor = .whiteDay
--- FakeNFT/Components/Buttons/BlackBaseButton.swift
+++ FakeNFT/Components/Buttons/BlackBaseButton.swift
@@ -2,17 +2,32 @@ import UIKit
 
 final class BaseBlackButton: UIButton {
     let labelText: String
-
+    
     init(with title: String) {
         self.labelText = title
         super.init(frame: .zero)
         setupUI()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        transform = .identity
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        transform = .identity
+    }
+    
     private func setupUI() {
         self.backgroundColor = .blackDay
         self.setTitle(labelText, for: .normal)
@@ -20,7 +35,7 @@ final class BaseBlackButton: UIButton {
         self.layer.cornerRadius = 16
         self.titleLabel?.textAlignment = .center
         self.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .bold)
-
+        
         NSLayoutConstraint.activate([
             self.heightAnchor.constraint(equalToConstant: 60)
         ])
