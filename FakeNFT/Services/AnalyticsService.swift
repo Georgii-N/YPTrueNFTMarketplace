37897dec902cd8f7564d67ab431d502d89325f39
Georgii <neguritsageorgiy@gmail.com>
2023-08-16 15:32:40 +0400

    Update AnalyticsService.swift

--- FakeNFT/Services/AnalyticsService.swift
+++ FakeNFT/Services/AnalyticsService.swift
@@ -25,6 +25,7 @@ enum Screens: String {
 
 enum Items: String {
     case screen = "Screen"
+    case collectionCell = "CollectionCell"
     case buttonSorting = "ButtonSorting"
     case buttonSortingByName = "ButtonSortingByName"
     case buttonSortingByRating = "ButtonSortingByRating"
